9c8e29b8e4d4be16800abe5a276dfe51578e1e99
Andy <hiandyzhang@gmail.com>
2016-09-28 00:29:35 -0700

    add mean uncomplete button

--- calculator.js
+++ calculator.js
@@ -8,13 +8,36 @@ $(document).ready(function() {
 			  $(".percent:eq("+i+")").html(Math.round(input1/ input2 * 100*100)/100+"%");
 			}
 		}
-	}
+	};
+
+	var mean = function(){
+		var per_counter = 0;//counter for valid percent
+		var result = 0;
+		for (i = 0 ; i < count; i++){
+			var per_i = parseFloat( $(".percent:eq("+i+")").val() );
+			if ( !isNaN(per_i) ){
+				per_counter ++;
+				result = result + per_i;
+			}
+		}
+		result = result / per_counter;
+		$("#mean_result").html(result);
+	};
+
 	$(".grade").keyup(function() {  
 	    calculate();
 	});
 
 	$(".total_grade").keyup(function() {  
 	    calculate();
+	});
+
+	// $("#weighted_button").click(function(){
+	// 	weight();
+	// });
+
+	$("#mean_button").click(function(){
+		mean();
 	});
 });
 
